279d208a2f6a66e9d0cfda8975d7285ab1a559bf
crakenpoop123 <clarkgerrard2@gmail.com>
2026-08-29 10:27:22 +1200

    Got the person appearing after a few seconds

--- scenes/person.gd
+++ scenes/person.gd
@@ -1,11 +1,13 @@
 extends TextureRect
 
-var truth_teller = true
+var truth_teller
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if randf() < 0.15:
 		truth_teller = false
+	else:
+		truth_teller = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 var grid_length
-
 var card = preload("res://scenes/card.tscn")
+
+var person_time = 2.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Person.visible = false
+	
 	grid_length = $CardsGrid.columns
 	
 	for y_pos in range(grid_length):
@@ -15,8 +18,14 @@ func _ready() -> void:
 			curr_card.card_pos = Vector2(x_pos, y_pos)
 			
 			$CardsGrid.add_child(curr_card)
+	
+	$PersonTimer.start(person_time)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_person_timer_timeout() -> void:
+	$Person.visible = !$Person.visible
+	
