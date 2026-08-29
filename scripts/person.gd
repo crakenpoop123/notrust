7a1411f8886f45525f77433857072d31708236d9
crakenpoop123 <clarkgerrard2@gmail.com>
2026-08-29 12:44:27 +1200

    Added a way to insert a value into the speech

--- scripts/person.gd
+++ scripts/person.gd
@@ -3,14 +3,14 @@ extends Sprite2D
 var truth_teller
 
 var text_possibilities = {
-	"Name": ["get_name"],
-	"NameList": ["Bob", "Hubert", "Clive", "Rosamond", "Karl", "Trenton", "Festus", "Jason", "Emmett", "Cheri", "Annice", "Fiona", "Ellery", "Leatrice", "Ursula", "Meryl", "Aura", "Vanessa", "Larissa", "Keri", "Mathilda", "Georgia", "Goldie", "Spring", "Cade", "Autumn", "Landon", "Garland", "Cecil", "Quincy", "Jeffery", "Lynwood", "Cordell", "Lela", "Wyatt"],
+	"Name": ["Bob", "Hubert", "Clive", "Rosamond", "Karl", "Trenton", "Festus", "Jason", "Emmett", "Cheri", "Annice", "Fiona", "Ellery", "Leatrice", "Ursula", "Meryl", "Aura", "Vanessa", "Larissa", "Keri", "Mathilda", "Georgia", "Goldie", "Spring", "Cade", "Autumn", "Landon", "Garland", "Cecil", "Quincy", "Jeffery", "Lynwood", "Cordell", "Lela", "Wyatt"],
 	"Height": ["range", 150, 220],
-	"Age": ["range", 24, 83],
+	"Age": ["range", 20, 80],
 	"Birthday": ["get_birthday"],
 	"BirthMonth": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
 	"BirthDay": [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-	"Speech": ["Believe me, a safe tile is at ϧ", "I'm not lying, there is a safe card at ϧ", "I wouldn't lie, the safe card is ϧ", "ϧ is safe, believe me", "YOU MUST LISTEN TO ME! ϧ is safe", "Trust me, ϧ is a safe card"],
+	"Speech": ["get_speech"],
+	"SpeechList": ["Believe me, a safe tile is at ϧ", "I'm not lying, there is a safe card at ϧ", "I wouldn't lie, the safe card is ϧ", "ϧ is safe, believe me", "YOU MUST LISTEN TO ME! ϧ is safe", "Trust me, ϧ is a safe card"],
 } # Replace ϧ (Alt + 999) with the spot for Speech
 
 @onready var text_location = $"../PersonText"
@@ -18,7 +18,8 @@ var text_possibilities = {
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if randf() < 0.15:
-		truth_teller = false
+		#truth_teller = false
+		truth_teller = true # TODO: Uncomment the false line above here to enable liars
 	else:
 		truth_teller = true
 	init_text()
@@ -26,11 +27,11 @@ func _ready() -> void:
 # Init the text variables
 func init_text():
 	for text in text_location.get_children():
-		print("text node: ", text)
-		print("text name: |", text.name, "|")
-		print("output: ", init_specific_text(text))
+		#print("text node: ", text)
+		#print("text name: |", text.name, "|")
+		#print("output: ", init_specific_text(text))
+		#print("----------")
 		text.text = str(text.name, ": ", init_specific_text(text))
-		print("----------")
 
 # Used so that I can exit when the text gets set
 func init_specific_text(text):
@@ -48,7 +49,7 @@ func init_specific_text(text):
 	
 	# Check if there is custom code for the text
 	if self.has_method(str(first_item)):
-		return call(first_item, true) # TODO: Replace with truth_teller when set up for liars
+		return call(first_item)
 	
 	# Check if it is a range
 	if first_item == "range":
@@ -62,14 +63,14 @@ func init_specific_text(text):
 	return "ERROR"
 
 # Get a valid brithday by pulling from BirthMonth and BirthDay
-func get_birthday(real):
+func get_birthday():
 	# Init the birthday text
 	var birthday_text = ""
 	
 	var months_max = 11 # 12 months in a month - 1 for zero-indexing
 	
 	# Generate a real birthday for the truth-tellers
-	if real:
+	if truth_teller:
 		# Get the index of a random month
 		var month_index = randi_range(0, months_max)
 		
@@ -87,7 +88,20 @@ func get_birthday(real):
 	
 	return birthday_text
 
-# Returns a random name, with ϧ replaced by a card location
+# Returns a random speech, with ϧ replaced by a card location
+func get_speech():
+	# Gets the unedited speech
+	var speech = text_possibilities["SpeechList"][randi_range(0, len(text_possibilities["SpeechList"]) - 1)]
+	
+	# Get the index of the filler character: ϧ
+	var filler_index = speech.find("ϧ")
+	
+	# Remove the filler value from the text
+	speech[filler_index] = ""
+	# Insert the new value into the text
+	speech = speech.insert(filler_index, str("TEST"))
+	
+	return speech
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
